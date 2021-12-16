625d7d7e4aad57b6144d51f424c5f15d8e4f57a9
Eduardo Torres Ferreira <eduardotorres@jera.com.br>
2021-12-16 17:58:06 -0400

    Added From Gallery

--- app/src/main/java/br/com/vendas/ui/screens/NewProducts.kt
+++ app/src/main/java/br/com/vendas/ui/screens/NewProducts.kt
@@ -12,18 +12,21 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import androidx.navigation.NavHostController
 import br.com.vendas.ui.theme.Sizes.P
 import br.com.vendas.ui.utils.Permission
 import br.com.vendas.ui.utils.camera.CameraCapture
+import br.com.vendas.ui.utils.camera.EMPTY_IMAGE_URI
+import br.com.vendas.ui.utils.camera.GallerySelect
 import br.com.vendas.ui.utils.permissionNotAvaiable
 import coil.compose.rememberImagePainter
 
 @Composable
 fun NewProducts(navController: NavHostController) {
     Column(modifier = Modifier.padding(P) ) {
-        pseudo()
+        pseudo2()
     }
 }
 
@@ -45,7 +48,7 @@ fun MainContent(modifier: Modifier = Modifier) {
 
 @Composable
 fun pseudo(modifier: Modifier = Modifier) {
-    val emptyImageUri = Uri.parse("file://dev/null")
+    val emptyImageUri = EMPTY_IMAGE_URI
     var imageUri by remember { mutableStateOf(emptyImageUri) }
     if (imageUri != emptyImageUri) {
         Box(modifier = modifier) {
@@ -70,5 +73,57 @@ fun pseudo(modifier: Modifier = Modifier) {
                 imageUri = file.toUri()
             }
         )
+    }
+}
+
+@Composable
+fun pseudo2(modifier: Modifier = Modifier){
+    val emptyImageUri = EMPTY_IMAGE_URI
+    var imageUri by remember { mutableStateOf(emptyImageUri) }
+    var showGallerySelect by remember { mutableStateOf(false) }
+    if (imageUri != emptyImageUri) {
+        Box(modifier = modifier) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = rememberImagePainter(imageUri),
+                contentDescription = "Captured image"
+            )
+            Button(
+                modifier = Modifier.align(Alignment.BottomCenter),
+                onClick = {
+                    imageUri = emptyImageUri
+                }
+            ) {
+                Text("Remove image")
+            }
+        }
+    }
+    if (showGallerySelect) {
+        GallerySelect(
+            modifier = modifier,
+            onImageUri = { uri ->
+                showGallerySelect = false
+                imageUri = uri
+            }
+        )
+    } else {
+        Box(modifier = modifier) {
+            CameraCapture(
+                modifier = modifier,
+                onImageFile = { file ->
+                    imageUri = file.toUri()
+                }
+            )
+            Button(
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .padding(4.dp),
+                onClick = {
+                    showGallerySelect = true
+                }
+            ) {
+                Text("Select from Gallery")
+            }
+        }
     }
 }
---
+++ app/src/main/java/br/com/vendas/ui/utils/camera/CameraConstants.kt
@@ -0,0 +1,5 @@
+package br.com.vendas.ui.utils.camera
+
+import android.net.Uri
+
+val EMPTY_IMAGE_URI: Uri = Uri.parse("file://dev/null")
